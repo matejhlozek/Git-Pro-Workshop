c18b1a499e3e6cc989113b787c06af76f2835c2d
Matej Nimmerrichter <nimmerrichter.matej@gmail.com>
2022-05-02 15:13:23 +0200

    b5

--- count.java
+++ count.java
@@ -1,5 +1,6 @@
 public static void count () {
 	System.out.print('9');
-public static void count (int age) {
-	System.out.prin(age);
+public static void count (int age, int currentYear) {
+	System.out.print(age);
+	System.out.print(currentYear)
 }
